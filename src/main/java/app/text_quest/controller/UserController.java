7a6ce0606ab9bfef807716ff568893d9cffebc03
T1MON <derbindima5@gmail.com>
2021-02-15 00:19:10 +0300

    Added test methods

--- src/main/java/app/text_quest/controller/UserController.java
+++ src/main/java/app/text_quest/controller/UserController.java
@@ -3,22 +3,24 @@ package app.text_quest.controller;
 
 import app.text_quest.model.User;
 import app.text_quest.service.UserService;
-import app.text_quest.util.modelCreator.PswCreator;
-import org.springframework.beans.factory.annotation.Autowired;
+import app.text_quest.util.modelCreator.types.PswFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    private final UserService userService;
 
-    @GetMapping("/users")
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
     public String getUsers(Model model) {
         List<User> userList = userService.getAll();
         model.addAttribute("names", new ArrayList<String>());
@@ -34,7 +36,7 @@ public class UserController {
     public User updateAnswer(@PathVariable String name,
                                @PathVariable String email) {
         User user = new User();
-        user.setPsw(PswCreator.create());
+        user.setPsw(new PswFactory().create());
         user.setEmail(email);
         user.setName(name);
         return userService.addUser(user);
