3596545f935d6a328e21b7516051ee0005b8aa01
T1MON <derbindima5@gmail.com>
2021-02-20 14:26:32 +0300

    Configuring deploying

--- src/main/java/app/text_quest/controller/RootController.java
+++ src/main/java/app/text_quest/controller/RootController.java
@@ -4,7 +4,8 @@ package app.text_quest.controller;
 import app.text_quest.util.FileLogger;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -12,7 +13,8 @@ public class RootController {
 
     private final Logger logger = Logger.getLogger("controllerLog");
 
-    @GetMapping("")
+    @RequestMapping("/")
+    @ResponseBody
     public String root() {
         try {
             return "index.min";
