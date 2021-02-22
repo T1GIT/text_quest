b075c24ad7d590c7a3f954653bad82cb90996ad7
T1MON <derbindima5@gmail.com>
2021-02-22 09:28:08 +0300

    Cleaned for deploy

--- src/main/java/app/text_quest/controller/RootController.java
+++ src/main/java/app/text_quest/controller/RootController.java
@@ -5,6 +5,9 @@ import app.text_quest.util.ExceptionLogger;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.File;
 
 
 @Controller
@@ -13,9 +16,24 @@ public class RootController {
     private final Logger logger = Logger.getLogger("controllerLogger");
 
     @GetMapping("/")
+    @ResponseBody
     public String root() {
         try {
-            return "index.min";
+            String path = "classpath:/";
+            File dir = new File(path);
+            for (File file1 : dir.listFiles()) {
+                System.out.println(file1);
+                if (file1.isDirectory() && file1.listFiles() != null) {
+                    for (File file2 : file1.listFiles()) {
+                        System.out.println(file2);
+                        if (file2.isDirectory() && file2.listFiles() != null) {
+                            for (File file3 : file2.listFiles()) {
+                                System.out.println(file3);
+                            }
+                        }
+                    }
+                }
+            }
         } catch (Exception e) {
             logger.error(ExceptionLogger.getExceptionLog(e));
         }
